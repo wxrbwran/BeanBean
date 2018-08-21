69c5218453372318c03844ec5f776b8e6747f34c
wuxiaoran <wxrbw@qq.com>
2018-08-21 18:16:32 +0800

    获取电影数据

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,10 +1,13 @@
 // pages/movie/movie.js
 const app = getApp();
+const util = require('../../utls/util.js')
+
 Page({
   data: {
     inTheaters: [],
     comingSoon: [],
     top250: [],
+    containerShow: false,
   },
   getMovieListData(url, settedKey, categoryTitle) {
     var that = this;
@@ -15,13 +18,41 @@ Page({
       "content-type": "json"
       },
       success(res) {
-        that.processDoubanData(res.data, settedKey, categoryTitle)
+        that.processDoubanData(res.data, settedKey, categoryTitle);
+        that.setData({ containerShow: true });
       },
       fail(error) {
         // fail
         console.log(error)
-      }
+      },
     })
+  },
+  processDoubanData(moviesDouban, settedKey, categoryTitle) {
+    const movies = [];
+    console.log(moviesDouban)
+    for (let subject of moviesDouban.subjects) {
+      // const subject = moviesDouban.subjects[idx];
+      let title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+      // [1,1,1,1,1] [1,1,1,0,0]
+      const temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    const readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      movies: movies
+    }
+    this.setData(readyData);
+    wx.hideNavigationBarLoading();
   },
   /**
    * 生命周期函数--监听页面加载
@@ -33,6 +64,7 @@ Page({
     const inTheatersUrl = `${doubanBase}${nameSpace}in_theaters${params}`;
     const comingSoonUrl = `${doubanBase}${nameSpace}coming_soon${params}`;
     const top250Url = `${doubanBase}${nameSpace}top250${params}`;
+    wx.showNavigationBarLoading();
     this.getMovieListData(inTheatersUrl, 'inTheaters', '正在上映');
     this.getMovieListData(comingSoonUrl, 'comingSoon', '即将上映');
     this.getMovieListData(top250Url, 'top250', '豆瓣250'); 
